e1be0f6e64072c9165edc4fe43ccf25492ea0286
WalterMarch <walter.march@outlook.com>
2024-02-01 08:56:44 +0000

    added comments to post-create

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,10 +2,12 @@
 
 source $1/.devcontainer/configit.sh $1
 
+# Python and Pip
 sudo apt-get update
 sudo apt-get install -y python3-pip
 pip3 install -U --user --upgrade pip
 pip3 install mypy -U --user
 
+# Jupyter and kernels
 pip3 install jupyter -U --user --force-reinstall 
 pip3 install ipykernel -U --user --force-reinstall
