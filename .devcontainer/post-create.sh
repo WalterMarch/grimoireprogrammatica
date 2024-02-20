3f3744e732643eaa667f772cdcb5173397e5b6cd
WalterMarch <walter.march@outlook.com>
2024-02-20 23:35:48 +0000

    removing python stuff from devcontainer files

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -6,14 +6,6 @@ source $1/.devcontainer/configit.sh $1
 sudo apt-get update
 sudo apt-get install -y tree
 
-# Pip and Python-related
-sudo apt-get install -y python3-pip
-pip3 install -U --user --upgrade pip
-pip3 install mypy -U --user
-pip3 install jupyter -U --user --force-reinstall 
-pip3 install ipykernel -U --user --force-reinstall
-sudo apt-get install tk -y
-
 # mySQL
 sudo apt-get install -y mysql-server
 sudo /etc/init.d/mysql start
