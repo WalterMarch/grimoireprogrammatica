e0e0c791e091e2f79f9f59a1da3444729c9f1ec3
WalterMarch <walter.march@outlook.com>
2024-02-21 23:24:30 +0000

    removed mysql stuff from post-create

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -5,8 +5,3 @@ source $1/.devcontainer/configit.sh $1
 # Linux utils
 sudo apt-get update
 sudo apt-get install -y tree
-
-# mySQL
-sudo apt-get install -y mysql-server
-sudo /etc/init.d/mysql start
-source $1/mysql/sakila-db/setup_sakila.sh
